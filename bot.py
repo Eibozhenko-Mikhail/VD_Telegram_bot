7907284a2ed48c3ea5c94eb5f6fb2938f0033328
Eibozhenko-Mikhail <mikhailerox@gmail.com>
2024-11-11 12:54:36 +0100

    Testing message response

--- bot.py
+++ bot.py
@@ -12,6 +12,6 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text) #Testing
 
 bot.infinity_polling()
